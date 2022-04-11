a4a1b061d6d8252063bf5efd1ed4ac8738faefd2
madhavirabbagungu@gmail.com <madhavirabbgundu@gmail.com>
2022-04-11 15:34:06 +0530

    submitted

---
+++ navbar_before_login.js
@@ -0,0 +1,27 @@
+function navbar() {
+    return `<div class="fixed_header">
+    <div class="navbar">
+        <div class="logo">
+            <img src="https://newassets.apollo247.com/images/ic_logo.png" alt="logo">
+        </div>
+        <div class="options">
+            <p><a href="Doctors.html">Doctors</a> <br> <span class="line"></span> <a>Consult <br> Online</a>
+            </p>
+            <p><a href="pharmacyy.html">Pharmacy</a> <br><span class="line"></span> <a>Medicnes & <br> other
+                    products</a> </p>
+            <p><a href="health_record.html">Health Record</a> <br> <span class="line"></span> <a> Health
+                    <br>check-ups</a> </p>
+            <p><a href="Blog.html">Blog</a> <br> <span class="line"></span> <a>Latest <br>Updates</a>
+            <p><a href="Covid_19.html">Covid 19</a> <br> <span class="line"></span> <a>Latest <br>Updates</a>
+            </p>
+            <img class="cart" src="cart.png" alt="">
+            <a onclick="openForm()"><img class="user-profile" src="./opoloimages/user-profilee.webp" alt="user-profile"></a>
+
+
+         </div>
+        </div>
+    </div>`;
+    
+}
+
+export default navbar;
